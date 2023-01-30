af40985e3cc8a97b6c19fa0ce39ace0b51c5e053
Tomasz Dzidek <t.dzidek@integra.pl>
2023-01-30 09:35:52 +0100

    ComplexIf

--- ComplexIf/ComplexIf.cs
+++ ComplexIf/ComplexIf.cs
@@ -1,24 +1,26 @@
 ﻿namespace ComplexIf;
 
 public record Customer(string Name, decimal TotalTurnover, decimal LastQuarterTurnover, bool HasLoyaltyCard);
+
 public class ComplexIf
 {
     private const decimal PremiumTotalTurnover = 1000000;
     private const decimal PremiumQuarterTurnover = 250000;
-    
+
     public void ComplexIf_Wrong(Customer customer)
     {
-        if (customer.HasLoyaltyCard && customer.TotalTurnover > PremiumTotalTurnover && customer.LastQuarterTurnover > PremiumQuarterTurnover)
+        if (IsPremium(customer))
         {
             //do something
         }
     }
 
-    private bool IsPremium(Customer customer)
+    private static bool IsPremium(Customer customer)
     {
-        return customer.HasLoyaltyCard && customer.TotalTurnover > PremiumTotalTurnover && customer.LastQuarterTurnover > PremiumQuarterTurnover;
+        return customer.HasLoyaltyCard && customer.TotalTurnover > PremiumTotalTurnover
+                                       && customer.LastQuarterTurnover > PremiumQuarterTurnover;
     }
-    
+
     public void ComplexIf_Good(Customer customer)
     {
         if (IsPremium(customer))
